3553d47f561ff43643660a57b52dc525f4a2ef33
Daria Vyrvich <dasvyry@gmail.com>
2021-10-11 15:47:26 +0300

    ..

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict"
+// 1.
+alert("Привет, это работа Вырвич Дарьи");
+// 2.
+const planetName = "Earth";
+let myAge = 22;
+alert(planetName + " " + myAge);
+// 3. 
+const myName = "Дарья";
+const mySurname = "Вырвич";
+let myyAge = 22;
+alert("Привет, меня зовут " + myName + " " + mySurname + ", мне " + myyAge + " год.");
+// 4. 
+let namee = prompt("Введите свое имя");
+alert("Мою работу проверяет " + namee);
+
+// 5.1 
+// let a = 1, b = 1;
+// let c = ++a; 
+// let d = b++;
+// alert("a = " + a + " " + "b = " + b + " "+ "c = " + c + " " + "d = " + d);
+
+// a = 2 
+// b = 2
+// c = 2
+// d = 1
+
+// 5.2
+let a = 3;
+let b = 2;
+let x = 1 + (a += 2);
+let y = 1 + (b *= 2);
+alert(a + " " + b + " " + x + " " + y);
+
+
+
+
+
+
+
